7107d0d99788c0ef512d6953e5550dd2074ac56c
Keisuke KOIKE <keisuke.koike@dena.jp>
2019-06-24 18:08:00 +0900

    wip question show endpoints

--- virtual_tutor/app/controllers/questions_controller.rb
+++ virtual_tutor/app/controllers/questions_controller.rb
@@ -1,8 +1,27 @@
 class QuestionsController < ApplicationController
+  def index
+    questions = Question.all
+    render json: questions, status: 200
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "見つかりませんでした" }, status: 404
+  end
+  
   def create
-    question = Question.create!(body: params[:body])
-    render json: { questionID: question.id, body: question.body }, status: 200
+    question = Question.create!(body: question_params[:body])
+    render json: question, status: 200
   rescue ActiveRecord::RecordInvalid
     render json: { error: "投稿ができませんでした" }, status: 422
+  end
+
+  def show
+    question = Question.joins(:answer).find(question_params[:id])
+    render json: question, status: 200
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "見つかりませんでした" }, status: 404
+  end
+
+  private
+  def question_params
+    params.permit(:id, :body)
   end
 end
--- virtual_tutor/db/schema.rb
+++ virtual_tutor/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_073225) do
+ActiveRecord::Schema.define(version: 2019_06_24_090050) do
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
