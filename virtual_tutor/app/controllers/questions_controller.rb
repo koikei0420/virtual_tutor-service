17822884ce764b8b9f8ba861ecf42b27405465e1
Keisuke KOIKE <keisuke.koike@dena.jp>
2019-06-25 11:02:17 +0900

    create question show endpoint

--- virtual_tutor/app/controllers/questions_controller.rb
+++ virtual_tutor/app/controllers/questions_controller.rb
@@ -3,21 +3,21 @@ class QuestionsController < ApplicationController
     questions = Question.all
     render json: questions, status: 200
   rescue ActiveRecord::RecordNotFound
-    render json: { error: "見つかりませんでした" }, status: 404
+    render json: { error: '見つかりませんでした' }, status: 404
   end
   
   def create
     question = Question.create!(body: question_params[:body])
     render json: question, status: 200
   rescue ActiveRecord::RecordInvalid
-    render json: { error: "投稿ができませんでした" }, status: 422
+    render json: { error: '投稿ができませんでした' }, status: 422
   end
 
   def show
-    question = Question.joins(:answer).find(question_params[:id])
+    question = Question.joins(:answers).where(answers: { question_id: question_params[:id] })
     render json: question, status: 200
   rescue ActiveRecord::RecordNotFound
-    render json: { error: "見つかりませんでした" }, status: 404
+    render json: { error: '見つかりませんでした' }, status: 404
   end
 
   private
